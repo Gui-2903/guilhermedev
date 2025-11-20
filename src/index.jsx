d9f0bc43a3423a1646465479b11a8ef4ed53e254
Guilherme <guilherme.1295232@discente.uemg.br>
2025-11-20 18:07:21 -0300

    pagina sem scrol lateral

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,7 @@ import './styles/Global.module.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
     <script type="module" src="/src/index.jsx"></script>
   </React.StrictMode>
   
